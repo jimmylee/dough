992e54ebcb9800f1f0a5215acaff58b9836231d6
pifafu <pifafu@github.com>
2016-08-07 23:58:43 -0700

    Add more utils and clean up pointless components

--- app/components/exhibit/index.js
+++ app/components/exhibit/index.js
@@ -23,26 +23,27 @@ export default React.createClass({
           <span className="exhibit-preview-thumbnail-item"></span>
           <span className="exhibit-preview-thumbnail-item"></span>
           <div className="exhibit-preview-description">
-            <span className="exhibit-preview-gallery mr-3">Thinkspace</span>
-            <span className="exhibit-preview-dates">Jun 2 – Jun 25</span>
+            <span className="dark text-default medium ls-sm mr-3">Thinkspace</span>
+            <span className="grey text-small ls-xs">Jun 2 – Jun 25</span>
           </div>
         </div>
-        <div className="exhibit-preview">
-          <div className="exhibit-preview-heading">
-            Exhibit name
-          </div>
+        <div className="exhibit-preview mt-6">
+          <span className="exhibit-preview-heading">
+            Summer Group Show 2016
+          </span>
           <div className="exhibit-preview-description">
-            <span className="exhibit-preview-gallery mr-3">Gallery</span>
-            <span className="exhibit-preview-dates">Dates showing</span>
+            <span className="dark text-default medium ls-sm mr-3">Hashimoto Contemporary</span>
+            <span className="grey text-small ls-xs">Jun 2 – Jun 25</span>
           </div>
         </div>
-        <div className="exhibit-preview">
-          <div className="exhibit-preview-heading">
-            Exhibit name
-          </div>
+        <div className="exhibit-preview mt-6">
+          <span className="exhibit-preview-heading">
+            Exhibit Title
+          </span>
+          <span className="exhibit-preview-thumbnail-item"></span>
           <div className="exhibit-preview-description">
-            <span className="exhibit-preview-gallery mr-3">Gallery</span>
-            <span className="exhibit-preview-dates">Dates showing</span>
+            <span className="dark text-default medium ls-sm mr-3">Gallery</span>
+            <span className="grey text-small ls-xs">Show dates</span>
           </div>
         </div>
       </div>
