ef089e52d64bf118a039f7874134bfd562dcff2c
pifafu <pifafu@github.com>
2016-08-07 23:43:36 -0700

    Basic styles for exhibits without images

--- app/components/exhibit/index.js
+++ app/components/exhibit/index.js
@@ -16,12 +16,15 @@ export default React.createClass({
         <div className="exhibit-thumbnail"></div>
         <div className="exhibit-thumbnail"></div>
         <div className="exhibit-preview mt-6">
-          <div className="exhibit-preview-heading">
-            Exhibit name
-          </div>
+          <span className="exhibit-preview-heading">
+            Vitality & Verve in the third dimension
+          </span>
+          <span className="exhibit-preview-thumbnail-item"></span>
+          <span className="exhibit-preview-thumbnail-item"></span>
+          <span className="exhibit-preview-thumbnail-item"></span>
           <div className="exhibit-preview-description">
-            <span className="exhibit-preview-gallery mr-3">Gallery</span>
-            <span className="exhibit-preview-dates">Dates showing</span>
+            <span className="exhibit-preview-gallery mr-3">Thinkspace</span>
+            <span className="exhibit-preview-dates">Jun 2 – Jun 25</span>
           </div>
         </div>
         <div className="exhibit-preview">
