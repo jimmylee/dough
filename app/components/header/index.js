683e399d6ec24e6a928f50d968c87da231c04864
pifafu <pifafu@github.com>
2016-10-02 17:10:56 -0700

    get rid of assistant

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -9,6 +9,9 @@ export default React.createClass({
         <h1 className="heading">
           Happy Birthday <span className="red">Jimmy</span> &#4113;
         </h1>
+        <span className="font-menlo text-small">
+          For your birthday this year, I decided to give you 24 hours of art and animation.
+        </span>
       </header>
     );
   }
